b3bfdc9adb932bfe390b7aa32bba27a3b260de6b
FeiQuantumSoftware <fei8@vt.edu>
2022-02-06 18:15:56 -0500

    John fixed all errors, right version

--- molecool/functions.py
+++ molecool/functions.py
@@ -1,72 +1,9 @@
 """Provide the primary functions."""
-#import this
 import os
 import numpy as np
 import matplotlib.pyplot as plt
 
 from mpl_toolkits.mplot3d import Axes3D
-
-
-def canvas(with_attribution=True):
-    """
-    Placeholder function to show example docstring (NumPy format).
-
-    Replace this function and doc string for your own project.
-
-    Parameters
-    ----------
-    with_attribution : bool, Optional, default: True
-        Set whether or not to display who the quote is from.
-
-    Returns
-    -------
-    quote : str
-        Compiled string including quote and optional attribution.
-    """
-
-    quote = "The code is but a canvas to our imagination."
-    if with_attribution:
-        quote += "\n\t- Adapted from Henry David Thoreau"
-    return quote
-
-def zen(with_attribution=True):
-    quote = """Beautiful is better than ugly.
-    Explicit is better than implicit.
-    Simple is better than complex.
-    Complex is better than complicated.
-    Flat is better than nested.
-    Sparse is better than dense.
-    Readability counts.
-    Special cases aren't special enough to break the rules.
-    Although practicality beats purity.
-    Errors should never pass silently.
-    Unless explicitly silenced.
-    In the face of ambiguity, refuse the temptation to guess.
-    There should be one-- and preferably only one --obvious way to do it. 
-    Although that way may not be obvious at first unless you're Dutch. 
-    Now is better than never.
-    Although never is often better than *right* now.
-    If the implementation is hard to explain, it's a bad idea.
-    If the implementation is easy to explain, it may be a good idea. 
-    Namespaces are one honking great idea -- let's do more of those!"""
-    if with_attribution:
-        quote += "\n\tTim Peters"
-    return quote
-
-
-
-def open_pdb(f_loc):
-    with open(f_loc) as f:
-        data = f.readlines() 
-    c = [] 
-    sym = [] 
-    for l in data:
-        if 'ATOM' in l[0:6] or 'HETATM' in l[0:6]:
-            sym.append(l[76:79].strip())
-            c2 = [float(x) for x in l[30:55].split()]
-            c.append(c2) 
-    coords = np.array(c) 
-    return sym, coords
 
 
 def calculate_distance(rA, rB):
@@ -220,26 +157,30 @@ atom_colors = {
 }
 
 
+
+def canvas(with_attribution=True):
+    """
+    Placeholder function to show example docstring (NumPy format).
+
+    Replace this function and doc string for your own project.
+
+    Parameters
+    ----------
+    with_attribution : bool, Optional, default: True
+        Set whether or not to display who the quote is from.
+
+    Returns
+    -------
+    quote : str
+        Compiled string including quote and optional attribution.
+    """
+
+    quote = "The code is but a canvas to our imagination."
+    if with_attribution:
+        quote += "\n\t- Adapted from Henry David Thoreau"
+    return quote
+
+
 if __name__ == "__main__":
     # Do something if this file is invoked on its own
-    print("MAIN")
     print(canvas())
-    print("step0")
-    benzene_file_path = os.path.join('data', 'xyz', 'benzene.xyz')
-    print("Step A")
-    benzene_symbols, benzene_coords = open_xyz(benzene_file_path)
-    print("Step B")
-    benzene_bonds = build_bond_list(benzene_coords)
-    print("Step C")
-    avg_sum = 0
-
-    for atoms, bl in benzene_bonds.items():
-        avg_sum += bl
-
-    avg_bl = avg_sum/len(benzene_bonds)
-
-    print('The average bond length is {}'.format(avg_bl))
-
-    benzene_fig = draw_molecule(benzene_coords, benzene_symbols, draw_bonds=benzene_bonds)
-    plt.savefig('benzene.png', dpi=300)
-    bond_histogram(benzene_bonds, save_location="benzene_histogram.png")
