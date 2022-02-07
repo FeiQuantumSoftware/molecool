9e54997b94797fd5caf730b602843295e178304b
FeiQuantumSoftware <fei8@vt.edu>
2022-02-07 14:31:22 -0500

    edit the style of open_pdb function

--- molecool/functions.py
+++ molecool/functions.py
@@ -7,7 +7,7 @@ from mpl_toolkits.mplot3d import Axes3D
 
 
 def calculate_distance(rA, rB):
-    
+
     """Calculate the distance between two points.
     
     Parameters
@@ -34,15 +34,35 @@ def calculate_distance(rA, rB):
 
 
 def open_pdb(file_location):
-    # This function reads in a pdb file and returns the atom names and coordinates.
+    """Open and rea coordinates and atom symbols from pdb file.
+    
+    The pdb file must specify the atom elements in the last column, and follow
+    the conventions outlined in the PDB format specification.
+
+    Parameters
+    ----------
+    file_location : str
+        The location of the pdb file to read in.
+
+    Returns
+    -------
+    coords : np.ndarray
+        The coordinates of the pdb file.
+    symbols : list
+        The atomic symbols of the pdb file.
+
+    """
+
     with open(file_location) as f:
         data = f.readlines()
 
     coordinates = []
     symbols = []
+
     for line in data:
         if 'ATOM' in line[0:6] or 'HETATM' in line[0:6]:
             symbols.append(line[76:79].strip())
+            
             atom_coords = [float(x) for x in line[30:55].split()]
             coordinates.append(atom_coords)
 
